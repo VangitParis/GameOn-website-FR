fdf21735a1116aced37ca6f3a2948367d00604fe
VangitParis <102918421+VangitParis@users.noreply.github.com>
2023-01-10 17:48:00 +0100

    commentaires et css

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -5,6 +5,7 @@ function toogle() {
 }
 toogle();
 
+////////////////////////////////////////////////////////////////////////////////////////////////////*/
 /*/-----------------------------/OUVERTURE OU FERMETURE DES MODALES/------------------------------/*/
 // constantes déclarées depuis le DOM
 const modalBackground = document.querySelector(".background");
@@ -18,7 +19,7 @@ modalButtonRegister.forEach((buttonRegister) =>
   buttonRegister.addEventListener("click", showModal)
 );
 
-// affiche la modal du formulaire ou de remerciement
+// affiche la modale du formulaire ou de remerciement
 function showModal() {
   modalBackground.style.display = "block";
   buttonCloseThanksModal.style.display = "none";
@@ -37,7 +38,7 @@ closeFormModal();
 
 //==================================== BLOCK thanksModal ============================+//
 
-// booleen de la modal de remerciement:  false = n'a jamais été ouverte, true = ouvre la modal
+// booleen de la modale de remerciement:  false = n'a jamais été ouverte, true = ouvre la modal
 modalAlreadyOpened = false;
 // récupération et création d'élément dans le DOM
 const modalBody = document.querySelector(".modal-body");
@@ -62,16 +63,17 @@ function showThanksModal() {
   thanksModal.appendChild(thanksModalCloseBtn);
   modalBody.appendChild(thanksModal);
 
-  // fais apparaitre la modal de remerciement avec display = block
+  // fais apparaitre la modale de remerciement avec display = block
   thanksModal.style.display = "block";
   buttonCloseModalFom.style.display = "none";
   buttonCloseThanksModal.style.display = "block";
   // Si le formulaire est valide, faire disparaitre le formulaire avec display = none
   form.style.display = "none";
 }
-//déclaration du bouton pour fermer la modal au click sur le X
+// déclaration du bouton pour fermer la modal au click sur le X
 const buttonCloseThanksModal = document.querySelector(".close-thanks-modal");
 buttonCloseThanksModal.addEventListener("click", () => closeThanksModal(true));
+
 // Fonction pour fermer la modale de remerciement au boutton X
 function closeThanksModal(shouldResetFormValue = false) {
   if (shouldResetFormValue) {
@@ -90,18 +92,22 @@ function closeThanksModal(shouldResetFormValue = false) {
   modalAlreadyOpened = false;
 }
 closeThanksModal();
-// Fermer la modal DE REMERCIEMENT après le click sur le boutton Fermer
+
+/* Fermer la modal DE REMERCIEMENT après le click sur le boutton Fermer
+ * en appelant la fonction closeThanksModal
+ * et passer le paramètre à true
+ */
 thanksModalCloseBtn.addEventListener("click", (e) => {
   closeThanksModal(true);
 });
 
-/*////////////-------------ALERTE SI FORMULAIRE NON COMPLET OU CONTENANT DES ERREURS--------------///////// */
+/*////////////////////////////////////////////////////////////////////////////////////////////*/
+/*//-------------ALERTE SI FORMULAIRE NON COMPLET OU CONTENANT DES ERREURS----------------// */
 
 // Je crée un élement "p" qui est un message d'alerte qui prévient l'utilisateur si le formulaire n'est pas totalement rempli
 let message = document.createElement("p");
-// Je rajout une couleur, l'alignement et la police de texte
+// Je rajoute une couleur et l'alignement
 message.style.color = "#FF4E60";
-// message.style.fontSize = "12px";
 message.style.textAlign = "center";
 // J'attribue une classe à l'élément p
 message.setAttribute("class", "alerte");
@@ -112,14 +118,15 @@ message.innerText = "Veuillez remplir tous les champs du formulaire";
 // J'applique le style sur none pour qu'il ne soit pas visible
 message.style.display = "none";
 
+/////////////////////////////////////////////////////////////////////////////////////////*/
 /*//-----------------------------------FORMULAIRE---------------------------------------//
-//////////////////////////////////////////////////////////////////////////////////////*/
+
 
 /* Je crée une constante qui rappelle tous les champs du formulaire
  * avec un tableau d'objets pour stocker les informations de champ de formulaire et
  * une boucle pour configurer les écouteurs d'événement pour chaque champ.
- * //Cela facilite l'ajout ou la suppression de champs de formulaire, car il suffit de modifier le tableau d'objets//  */
-// const inputIds = ['firstName', 'lastName', 'email', 'birthdate', 'quantity']
+ * Cela facilite l'ajout ou la suppression de champs de formulaire, car il suffit de modifier le tableau d'objets
+ */
 const formFields = [
   {
     inputId: "firstName",
@@ -151,24 +158,32 @@ const formFields = [
     variableName: "isConditionsChecked",
     validateFunction: isInputValid,
   },
-
 ];
 //Pour chaque champ j'écoute l'évènement
+// j'initialise la checkbox à true car elle est déjà sélectionnée sur le front
 let isConditionsChecked = true;
+/* je boucle sur tous les champs du formulaire avec la méthode forEach() et
+ * je récupère tous les inputs grâce à leur id
+ */
 formFields.forEach((field) => {
   const inputDom = document.getElementById(field.inputId);
   window[field.variableName] = inputDom.value;
   inputDom.addEventListener("input", (e) => {
+    //Si le champ qu'on écoute est une checkbox
     if (field.inputId === "checkbox1") {
       window[field.variableName] = e.target.checked;
-    } else {
+    } //sinon on écoute les champs qui ont une value
+    else {
       window[field.variableName] = e.target.value;
     }
-
+    // ici on appelle la fonction qui valide le champ
     field.validateFunction(field.inputId);
     message.style.display = "none";
   });
 });
+/* De la même manière on écoute les boutons radio
+ * ici on crée une instance d'Array pour récupérer tous les inputs qui ont pour name = location
+ */
 let selectLocation = "";
 const selectLocationInputDom = Array.from(
   document.querySelectorAll('input[name="location"]')
@@ -181,10 +196,10 @@ selectLocationInputDom.forEach((location) =>
     message.style.display = "none";
   })
 );
-/*/==============================================================================================//
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
 // =============================VALIDATION FORMULAIRE========================================== /*/
 
-//Validation Formulaire si il est complété et vérifié sans erreurs
+//Soumission du Formulaire si il est complété et vérifié sans erreurs
 
 function submitForm() {
   // Ajout d'un écouteur d'évènement "submit" sur le formulaire
@@ -192,8 +207,11 @@ function submitForm() {
     // Empêche l'envoi du formulaire
     e.preventDefault();
 
-    // Vérification de la validité de tous les champs du formulaire
-    let areAllInputsValid = [
+    /* Vérification de la validité de tous les champs du formulaire et
+     * renvoi true si ils sont tous vérifiés et valides,
+     * Si c'est le cas on pourra soumettre le formulaire valide
+     */
+    let formIsValid = [
       firstName,
       lastName,
       birthDate,
@@ -203,8 +221,8 @@ function submitForm() {
       isConditionsChecked,
     ].every((input) => input);
 
-    // Si tous les champs du formulaire sont valides, affiche une boîte de dialogue de remerciement
-    if (areAllInputsValid) {
+    // Si tous les champs du formulaire sont valides, affiche la modale de remerciement
+    if (formIsValid) {
       // Ne pas afficher le message d'alerte
       message.style.display = "none";
       // Afficher le contenu de la modale de remerciement
@@ -219,21 +237,22 @@ function submitForm() {
       for (const formField of formFields) {
         isInputValid(formField.inputId);
       }
-      //formFields.forEach(formField => isInputValid(formField.inputId))
       isSelectLocationValid(selectLocationInputDom);
-
       return false;
     }
   });
 }
 submitForm();
-/*/============================ Validate Inputs Fields ============================== /*/
 
+/*///////////////////////////////////////////////////////////////////////////////////////////////////*/
+/*/============================ VALIDATION DES CHAMPS DU FORMULAIRE ============================== /*/
+
+//Fonction générique qui vérifie la validité de tous les champs et renvoie des erreurs si IsInputValid est false
 function validateField(field) {
   let errorMessage = "";
   let isInputValid = false;
 
-  console.info(field)
+  //Vérifie les champs prénom et nom
   if (field.value === "") {
     isInputValid = false;
     errorMessage = "Veuillez remplir ce champ.";
@@ -247,6 +266,7 @@ function validateField(field) {
         "Le champ ne doit pas contenir de chiffres ou de caractères spéciaux";
     }
   } else if (field.type === "email") {
+    //Vérifie le champ email
     if (
       !field.value.match(/^((?!\.)[\w_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/)
     ) {
@@ -257,7 +277,12 @@ function validateField(field) {
       errorMessage = "";
     }
   } else if (field.type === "date") {
-    if (isNaN(Date.parse(field.value))) {
+    /* Vérifie si la date de naissance est renseignée
+     * Number.isNan vérifie si la valeur entrée est de type number et renverra true sinon renverra false
+     * Si la chaîne représente une date valide, la méthode parse()renvoie une valeur numérique qui représente le nombre
+     * de millisecondes délivrées depuis le 1er janvier 1970.
+     */
+    if (Number.isNaN(Date.parse(field.value))) {
       isInputValid = false;
       errorMessage = "Vous devez entrer votre date de naissance.";
     } else {
@@ -285,7 +310,9 @@ function validateField(field) {
         errorMessage = "";
       }
     }
-  } else if (field.type === "number") {
+  }
+  //Vérifie si un nombre est renseigné
+  else if (field.type === "number") {
     // Convertir tournamentQuantity en entier avant de vérifier si c'est un entier
     const tournamentQuantityInt = parseInt(field.value, 10);
     // Vérifie si la quantité de tournois est un entier et supérieur ou égal à 1
@@ -294,7 +321,7 @@ function validateField(field) {
       errorMessage =
         "Veuillez entrer un entier supérieur ou égal à 1 pour la quantité de tournois.";
     }
-    // Vérifie si la quantité de tournois est vide
+    //Vérifie si la quantité de tournois est vide
     else if (field.value === "") {
       errorMessage =
         "Veuillez entrer la quantité de tournois auquel vous avez participé.";
@@ -311,6 +338,7 @@ function validateField(field) {
       errorMessage = "";
     }
   } else if (field.type === "checkbox") {
+    //Vérifie si les conditions sont acceptées
     if (!field.checked) {
       errorMessage =
         "Vous devez vérifier que vous acceptez les termes et conditions.";
@@ -319,6 +347,7 @@ function validateField(field) {
   return { isInputValid, errorMessage: errorMessage };
 }
 
+// Je crée une fonction pour gérer les erreurs et les afficher si les champs ne sont pas valides
 function updateError(field, isInputValid, errorMessage) {
   const errorElement = field.parentNode;
   errorElement.setAttribute("data-error", errorMessage);
@@ -328,7 +357,7 @@ function updateError(field, isInputValid, errorMessage) {
   );
   isInputValid = true;
 }
-
+// Je crée une fonction qui vérifie que les champs sont valides et sinon j'appelle la fonction updateError()
 function isInputValid(inputId) {
   const inputField = document.getElementById(inputId);
   const inputValidity = validateField(inputField);
@@ -338,7 +367,7 @@ function isInputValid(inputId) {
     inputValidity.errorMessage
   );
 }
-
+//Je crée une fonction exceptionnelle pour gérer le champ des bouttons radio
 //Validation du choix de la ville de jeu
 function isSelectLocationValid() {
   const locationField = document.querySelector("input[name=location]");
